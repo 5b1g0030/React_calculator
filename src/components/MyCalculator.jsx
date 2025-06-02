2cc1b38cce4f445e66153e43df9417a12acc3f9d
5b1g0030 <5b1g0030@stust.edu.tw>
2025-06-02 10:45:14 +0800

    添加註解

--- src/components/MyCalculator.jsx
+++ src/components/MyCalculator.jsx
@@ -7,19 +7,27 @@ function MyCalculator() {
     const [result, setResult] = useState("");
 
     const handleButtonClick = (value) => {
+        // 如果按下'C'，清空所有文字
         if (value === "C") {
             setExpression("");
             setResult("");
-        } else if (value === "←") {
-            setExpression(expression.slice(0, -1));
-        } else if (value === "=") {
+        } 
+        // 如果按下'←'，刪除最尾端的字元
+        else if (value === "←") { 
+            setExpression(expression.slice(0, -1)); // 回傳第0個~倒數第1個字元(不包含)
+        }
+        // 如果按下'='，開始計算輸入的算式
+        else if (value === "=") {
             try {
+                // 符號轉換: x->* , ÷->/
                 const evalResult = eval(expression.replace(/×/g, "*").replace(/÷/g, "/"));
-                setResult(evalResult);
+                setResult(evalResult); // 儲存運算結果
             } catch {
                 setResult("錯誤!");
             }
-        } else {
+        }
+        // 如果按下的按紐不是以上幾種，一律加在原字串後面
+        else {
             setExpression(expression + value);
         }
     };
